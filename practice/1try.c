92c28f6698a8f2abe5414450ad059766e94b4e47
Seungwoo Kim <seunkim@c4r2s8.42seoul.kr>
2020-10-08 15:09:56 +0900

    except cd and execve

---
+++ practice/1try.c
@@ -0,0 +1,135 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#include <stdio.h>
+
+typedef struct		s_cmd
+{
+	char			**args;
+	int				is_pipe;
+	int				fd[2];
+	struct s_cmd	*prev;
+	struct s_cmd	*next;
+}					t_cmd;
+
+int		ft_strlen(char *s)
+{
+	int	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	ft_putstr(char *s)
+{
+	write(2, s, ft_strlen(s));
+}
+
+void	exit_fatal()
+{
+	ft_putstr("error: fatal\n");
+	exit(1);
+}
+
+char	*ft_strdup(char *s)
+{
+	int 	i = 0;
+	char	*ptr;
+	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s) + 1))))
+		exit_fatal();
+	while (s[i])
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = 0;
+	return (ptr);
+}
+
+void	clear(t_cmd *cmd)
+{
+	t_cmd	*tmp;
+	int		i = 0;
+	while (cmd)
+	{
+		while (cmd->args[i])
+		{
+			free(cmd->args[i]);
+			i++;
+		}
+		free(cmd->args);
+		tmp = cmd;
+		cmd = cmd->next;
+		free(tmp);
+	}
+}
+
+t_cmd	*create_cmd(t_cmd *tmp, char **argv, int argv_num, int is_pipe)
+{
+	t_cmd	*new;
+	int		i = 0;
+	if (!(new = malloc(sizeof(t_cmd))))
+		exit_fatal();
+	if (!(new->args = malloc(sizeof(char *) * (argv_num + 1))))
+		exit_fatal();
+	while (i < argv_num)
+	{
+		new->args[i] = ft_strdup(argv[i]);
+		i++;
+	}
+	new->args[i] = NULL;
+	new->is_pipe = is_pipe;
+	new->prev = tmp;
+	new->next = NULL;
+	if (tmp)
+		tmp->next = new;
+	return (new);
+}
+
+int main(int argc, char **argv)
+{
+	t_cmd	*cmd;
+	t_cmd	*tmp;
+	int		start = 1;
+	int		last = 1;
+	int		is_pipe = 0;
+	int		res = 0;
+	int		i = 0;
+	
+	while (last < argc)
+	{
+		if (!strcmp(argv[last], "|") || !strcmp(argv[last], ";") || last + 1 == argc)
+		{
+			if (!strcmp(argv[last], "|"))
+				is_pipe = 1;
+			else if (!strcmp(argv[last], ";"))
+				is_pipe = 0;
+			else
+			{
+				is_pipe = 0;
+				last++;
+			}
+			if (last - start != 0)
+			{
+				tmp = create_cmd(tmp, &argv[start], last - start, is_pipe);
+				if (!cmd)
+					cmd = tmp;
+			}
+			start = last + 1;
+		}
+		last++;
+	}
+	while (cmd)
+	{
+		i = 0;
+		while (cmd->args[i])
+		{
+			printf("%s\n", cmd->args[i]);
+			i++;
+		}
+		cmd = cmd->next;
+	}
+	clear(cmd);
+}
